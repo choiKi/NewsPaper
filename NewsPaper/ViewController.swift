4ab94345e05b64287ccb902cf7ac437ca1e89509
choikihun <rlgns923@naver.com>
2022-02-12 12:36:21 +0900

    Initial Commit

---
+++ NewsPaper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsPaper
+//
+//  Created by 최기훈 on 2022/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
